7677a96ea3db6c201b72b52360c9518d41359d9a
Hublack <71221641+HublackBE@users.noreply.github.com>
2025-05-03 16:56:56 +0200

    feat: add automatic loading of most popular movies on successful authentication

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import './style.css'
 
-const url = 'https://api.themoviedb.org/3/authentication';
 const options = {
   method: 'GET',
   headers: {
@@ -9,14 +8,34 @@ const options = {
   }
 };
 
-const checkSuccess = json => {
-    if (!json.success) {
-        document.querySelector('#app').innerHTML += `<h1>Auth Error</h1><p>${JSON.stringify(json)}</p>`;
-    } else {
-        document.querySelector('#app').innerHTML += `<h1>Auth Success</h1>`;
-    }
+const loadPopular = page => {
+  const url = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&region=BE';
+
+  fetch(url, options)
+    .then(res => res.json())
+    .then(json => {
+      document.querySelector('#app').innerHTML = `<div id="movieList"></div>`;
+      const movieList = document.querySelector("#movieList");
+      for (const movie of json.results) {
+        console.log(movie);
+        movieList.innerHTML += `<div class='movie'><img src='https://image.tmdb.org/t/p/w500${movie.poster_path}'>`
+      }
+    })
+    .catch(err => console.error(err));
+
 }
 
+
+const checkSuccess = json => {
+  if (!json.success) {
+    document.querySelector('#app').innerHTML += `<h1>Auth Error</h1><p>${JSON.stringify(json)}</p>`;
+  } else {
+    document.querySelector('#app').innerHTML += `<h1>Auth Success</h1>`;
+    loadPopular(1);
+  }
+}
+
+const url = 'https://api.themoviedb.org/3/authentication';
 fetch(url, options)
   .then(res => res.json())
   .then(json => checkSuccess(json))
