e0d2d47c98833aa2120b7fc6ea107194fbaaf70d
Hublack <71221641+HublackBE@users.noreply.github.com>
2025-05-05 18:20:29 +0200

    feat: add buttons to bring to streaming platform
    TODO: make buttons only appear when available on said streaming platform

--- src/main.js
+++ src/main.js
@@ -23,16 +23,55 @@ const loadPopular = page => {
       const movieList = document.querySelector("#movieList");
       for (const movie of json.results) {
         console.log(movie);
-        movieList.innerHTML += `<div class='movie' id='${movie.id}'><img src='https://image.tmdb.org/t/p/w500${movie.poster_path}'><button class='watchButtonNetflix' type='Button'>Netflix</button></div>`
+        movieList.innerHTML += `
+        <div class='movie' id='${movie.id}'>
+        <img src='https://image.tmdb.org/t/p/w500${movie.poster_path}'>
+        <div class="watchButtons">
+        <button class='watchButtonNetflix' type='Button' value='${movie.id}'><img src="/Netflix.svg" alt="Netflix"></img></button>
+        <button class='watchButtonPrimeVideo' type='Button' value='${movie.id}'><img src='/PrimeVideo.svg' alt='Prime Video'></img></button>
+        <button class='watchButtonDisney' type='Button' value='${movie.id}'><img src='/Disney.png' alt='Disney+'></img></button>
+        </div></div>`
+        // TODO: make buttons only appear when available on said streaming platform
       }
+
       for (const movie of document.querySelectorAll(".movie")) {
         movie.addEventListener('click', () => {
           showDetails(movie.id);
         })
       }
+
+      for (const NetflixButton of document.querySelectorAll(".watchButtonNetflix")) {
+        NetflixButton.addEventListener('click', () => {
+          goToStreaming(NetflixButton.value, 'netflix');
+          console.log('Clicked!');
+        })
+      }
+
+      for (const PrimeVideoButton of document.querySelectorAll(".watchButtonPrimeVideo")) {
+        PrimeVideoButton.addEventListener('click', () => {
+          goToStreaming(PrimeVideoButton.value, 'prime');
+          console.log('Clicked!');
+        })
+      }
     })
     .catch(err => console.error(err));
+}
 
+const goToStreaming = async (id, streamingPlatform) => {
+  const RapidAPIURL = `https://streaming-availability.p.rapidapi.com/shows/movie/${id}`
+
+  fetch(RapidAPIURL, {method: 'GET', headers: { 'x-rapidapi-key': import.meta.env.VITE_RAPID_API_KEY,'x-rapidapi-host': 'streaming-availability.p.rapidapi.com'}})
+    .then((res) => res.json())
+    .then((returnJSON => {console.log(returnJSON); return returnJSON.streamingOptions.be}))
+    .then((platformsBE => {
+      console.log(platformsBE)
+      for (let platform of platformsBE) {
+        if (platform.service.id == streamingPlatform && platform.type == "subscription") {
+          window.open(platform.link);
+        }
+      }
+    }))
+    .catch(err => console.error(err))
 }
 
 const showDetails = async (id) => {
