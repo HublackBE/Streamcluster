c701b14617c2da360fbe2b7e9c5abf28f5479fcb
Hublack <71221641+HublackBE@users.noreply.github.com>
2025-05-12 10:56:29 +0200

    build: modify vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
+import { dirname    , resolve} from 'node:path'
+import { fileURLToPath} from 'node:url'
 import { defineConfig } from "vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 
 export default defineConfig({
     server: {
         port: 3000
     },
     build: {
-        outDir: 'dist'
+        outDir: 'dist',
+
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                search: resolve(__dirname, 'search/index.html'),
+            }
+        }
     }
 })
