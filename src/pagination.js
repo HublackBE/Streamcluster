5a2aed0c53d3df2fcf16fd54e03b28d2db7775f4
Hublack <71221641+HublackBE@users.noreply.github.com>
2025-05-13 11:59:38 +0200

    feat: add functioning previous and next button

--- src/pagination.js
+++ src/pagination.js
@@ -1,13 +1,34 @@
 export const createPagination = (json) => {
     const total_pages = json.total_pages;
 
+    mapPaginationButtons();
+}
+
+const mapPaginationButtons = () => {
     const paginationButtons = document.querySelectorAll('.paginationButton');
+    const previousButton = document.querySelector(`#previousPage`);
+
+    const url = new URL(window.location.href);
+    const currentPage = Number(url.searchParams.get(`page`));
 
     for (const button of paginationButtons) {
         button.addEventListener('click', () => {
-            let url = new URL(window.location.href);
             url.searchParams.set('page', button.innerHTML);
             window.location.href = url;
         })
     }
+
+
+    if (currentPage <= 1) {
+        previousButton.disabled = true;
+    }
+
+    previousButton.addEventListener(`click`, () => {
+        url.searchParams.set(`page`, (currentPage - 1 <= 0 ? 1 : currentPage - 1));
+        window.location.href = url;
+    })
+    document.querySelector(`#nextPage`).addEventListener('click', () => {
+        url.searchParams.set(`page`, currentPage == 0 ? 2 : currentPage + 1);
+        window.location.href = url;
+    })
 }
