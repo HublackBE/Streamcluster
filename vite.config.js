d67f3cdd6bd1b8a5f6976a130da172705c8ab81d
Hublack <71221641+HublackBE@users.noreply.github.com>
2025-05-20 20:22:41 +0200

    chore: fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,6 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
-                search: resolve(__dirname, 'search/index.html'),
             }
         }
     }
